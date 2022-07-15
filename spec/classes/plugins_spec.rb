d3aa9ef077fe86b1da1ab657bc3de1024d3a0f64
Joshua Hoblitt <josh@hoblitt.com>
2022-07-15 09:23:10 -0700

    add spec coverage for all metadata.json supported OS

--- spec/classes/plugins_spec.rb
+++ spec/classes/plugins_spec.rb
@@ -3,34 +3,34 @@
 require 'spec_helper'
 
 describe 'cni::plugins' do
-  let(:facts) do
-    {
-      os: {
-        family: 'RedHat',
-      },
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
 
-  describe 'without any parameters' do
-    it { is_expected.to compile.with_all_deps }
+      describe 'without any parameters' do
+        it { is_expected.to compile.with_all_deps }
 
-    it { is_expected.to have_cni__plugins__enable_resource_count(0) }
-  end
+        it { is_expected.to have_cni__plugins__enable_resource_count(0) }
+      end
 
-  describe 'with enable param' do
-    let(:params) do
-      {
-        'enable' => %w[
-          macvlan
-          dhcp
-        ],
-      }
+      describe 'with enable param' do
+        let(:params) do
+          {
+            'enable' => %w[
+              macvlan
+              dhcp
+            ],
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_cni__plugins__enable('macvlan') }
+        it { is_expected.to contain_cni__plugins__enable('dhcp') }
+        it { is_expected.to have_cni__plugins__enable_resource_count(2) }
+      end
     end
-
-    it { is_expected.to compile.with_all_deps }
-
-    it { is_expected.to contain_cni__plugins__enable('macvlan') }
-    it { is_expected.to contain_cni__plugins__enable('dhcp') }
-    it { is_expected.to have_cni__plugins__enable_resource_count(2) }
   end
 end
